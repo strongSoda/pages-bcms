a56258d67a593b29368f2f0af7f6e1419dd1f1d6
Imran <strongsoda2@gmail.com>
2018-08-18 19:41:10 +0530

    i didnt keep track

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var butter = require('buttercms')('1f289135f7c46e235ad2c9923f1b6426d7d5c9a3');
-var engine = require('ejs-locals');
 
 var app = express();
 
@@ -8,18 +7,31 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 // Routes
-app.get('/:git01', function() {
-  butter.page.retrieve("*", "Git01").then(function(resp) {
-    var page = resp.data.data;
 
-    res.render('Git01', {
-        coursepage : page.fields
+app.get('/', function(request, response) {
+  
+  butter.page.retrieve("*", "coursepage").then(function(res) {
+    try{
+    var page = res.data.data;
+  }catch(e) {
+    console.log(e + "ignoring");
+  }
+  
+    console.log(res);
+    response.render('coursepage', {
+        coursepage : page.fields,
     })
-    .catch(function (error) {
-        console.log(error);
-        done();
-      });
   });
 });
 
-app.listen(process.env.PORT || 7000);
+
+
+app.listen(process.env.port || 7000)
+
+// process.on('unhandledRejection', (reason, promise) => {
+//   console.log('Unhandled Rejection at:', reason.stack || reason)
+//   Promise.reject(new Error('This is fine'))
+//   // Recommended: send the information to sentry.io
+//   // or whatever crash reporting service you use
+// })
+
